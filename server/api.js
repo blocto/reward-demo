d936b3933929d71f0e0cc63979b62508a0c12444
Hsuan Lee <boczeratul@gmail.com>
2020-05-22 15:57:23 +0800

    Add instructions to README

--- server/api.js
+++ server/api.js
@@ -3,11 +3,10 @@ const fetch = require('node-fetch');
 const bodyParser = require('body-parser');
 
 const logger = require('./logger');
+const { TOKEN } = require('./constants');
 
 const app = express();
 app.use(bodyParser.json());
-
-const token = 'eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NfdG9rZW5faWQiOiIwMDAwMDAwMC0wMDAwLTAwMDAtMDAwMC0wMDAwMDAwMDAwMDAiLCJkYXBwX2lkIjoiMTk2NmZiNGQtNTQ0OS00MmM4LTliOGMtMjhkYmVlMjU5NmRmIiwiZGV2aWNlX2lkIjoiMDAwMDAwMDAtMDAwMC0wMDAwLTAwMDAtMDAwMDAwMDAwMDAwIiwiaW5mbyI6IiIsInNjb3BlcyI6WyIvcmV3YXJkL3NlbmQiXSwic2tpcF9hY2Nlc3NfdG9rZW4iOnRydWUsInVzZXJfaWQiOiIwMTY4YTU2Yi05MGQ5LTQ4MTUtYWI2Yy1lMzJhNDdmMDE1MWEifQ.8dAnuqzFKGl8SJ9YVbyFIlWE-0U0kCosmdnblQYNgm_1_U_oagI9PqNwpSEJxeXN';
 
 app.post(
   '/send',
@@ -19,7 +18,7 @@ app.post(
       {
         method: 'POST',
         headers: {
-          'Authorization': token,
+          'Authorization': TOKEN,
           'Content-Type': 'application/json'
         },
         body: JSON.stringify({
